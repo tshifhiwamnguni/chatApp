b087ca3e0b699b908f754e5313b3742fba3b8eaf
tshifhiwa <tshifhiwa.mnguni@dadevs.co.za>
2023-02-28 13:47:38 +0200

    restructure v1

--- src/components/chatHead/ChatHead.jsx
+++ src/components/chatHead/ChatHead.jsx
@@ -1,20 +1,27 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { IoIosCall } from "react-icons/io";
 import { BsCameraVideoFill } from "react-icons/bs";
-
+import second from './../../assests/WP1.jpeg'
 import SettingsDropdown from "../settingsDropdown/SettingsDropdown";
 
 function ChatHead() {
+const [name , setName] = useState()
+const names = ['koki', 'ipile', 'wethu', 'kgopo']
+    useEffect(()=>{
+        setName(names[parseInt(1 + Math.random() * (4 - 1))]);
+
+    },[])
+    console.log(name);
   return (
     <div className="head">
           <div className="left-panel">
             <div className="icon_back">
               <BiArrowBack className="icon_back" />
             </div>
-            <img src="" alt="" />
+            <img src={second} alt="" />
             <div className="user_details">
-              <div>ANGEL</div>
+              <div>{name}</div>
               <div>online</div>
             </div>
           </div>
